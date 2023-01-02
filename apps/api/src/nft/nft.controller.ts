ed9a6a516e1631049528f83e252f4cd42d5ce8c2
Johanna Fransson <adrina0002@gmail.com>
2023-01-02 16:34:17 +0100

    remove unused imports

--- apps/api/src/nft/nft.controller.ts
+++ apps/api/src/nft/nft.controller.ts
@@ -1,5 +1,4 @@
 import { Controller, Get, Param } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 import { Nft } from 'alchemy-sdk';
 import { NftService } from './nft.service';
 
