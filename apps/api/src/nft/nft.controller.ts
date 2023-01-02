91102fce12d544fd135d482ae3a7fce3a0ad9abe
Johanna Fransson <adrina0002@gmail.com>
2023-01-02 16:33:04 +0100

    added support for integrating swagger api docs generation

--- apps/api/src/nft/nft.controller.ts
+++ apps/api/src/nft/nft.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiProperty } from '@nestjs/swagger';
 import { Nft } from 'alchemy-sdk';
 import { NftService } from './nft.service';
 
 @Controller('nft')
 export class NftController {
-  constructor(private readonly nftService: NftService) {}
+  constructor(private readonly nftService: NftService) { }
 
   @Get(':address')
   async getNfts(@Param('address') address: string): Promise<Nft[]> {
