df0c460d3389b40fb3a30d4034a29428624840cc
Johanna Fransson <adrina0002@gmail.com>
2023-01-12 15:35:19 +0000

    added serviceworker configs

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const { InjectManifest } = require("workbox-webpack-plugin");
+/* const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   reactStrictMode: true,
@@ -18,5 +18,41 @@ module.exports = {
 
       return config;
     },
+  },
+};
+ */
+
+const { InjectManifest } = require("workbox-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const tsLoader = require("ts-loader");
+
+module.exports = {
+  // ... other configs
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (!isServer) {
+      config.plugins.push(
+        new InjectManifest({
+          swSrc: "./pages/service-worker.ts",
+          swDest: "../public/sw.js",
+          include: ["__nothing__"],
+        })
+      );
+    }
+    config.module.rules.push({
+      test: /\.ts$/,
+      use: [
+        {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({ configFile: "./tsconfig.json" }),
+    ];
+    config.resolve.extensions.push(".ts");
+    return config;
   },
 };
