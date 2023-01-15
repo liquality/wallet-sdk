3607dc61559dcd1550413a514a88675e459af77d
Johanna Fransson <adrina0002@gmail.com>
2023-01-15 10:54:41 +0000

    fix 'you may need a loader' issue with web package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,6 @@
-/* const { InjectManifest } = require("workbox-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const tsLoader = require("ts-loader");
 
 module.exports = {
   reactStrictMode: true,
@@ -10,49 +12,27 @@ module.exports = {
         config.plugins.push(
           new InjectManifest({
             swSrc: "./pages/service-worker.ts",
-            swDest: "./public/sw.js",
+            swDest: "../public/sw.js",
             include: ["__nothing__"],
           })
         );
       }
-
+      config.module.rules.push({
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+      });
+      config.resolve.plugins = [
+        new TsconfigPathsPlugin({ configFile: "./tsconfig.json" }),
+      ];
+      config.resolve.extensions.push(".ts");
       return config;
     },
-  },
-};
- */
-
-const { InjectManifest } = require("workbox-webpack-plugin");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const tsLoader = require("ts-loader");
-
-module.exports = {
-  // ... other configs
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    if (!isServer) {
-      config.plugins.push(
-        new InjectManifest({
-          swSrc: "./pages/service-worker.ts",
-          swDest: "../public/sw.js",
-          include: ["__nothing__"],
-        })
-      );
-    }
-    config.module.rules.push({
-      test: /\.ts$/,
-      use: [
-        {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-          },
-        },
-      ],
-    });
-    config.resolve.plugins = [
-      new TsconfigPathsPlugin({ configFile: "./tsconfig.json" }),
-    ];
-    config.resolve.extensions.push(".ts");
-    return config;
   },
 };
