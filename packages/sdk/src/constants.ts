f7298d84d61eab9f8acf3e53e8ad516ace0b4097
koderholic <22542946+koderholic@users.noreply.github.com>
2023-01-24 14:46:05 +0100

    refactor: rename variable

--- packages/sdk/src/constants.ts
+++ packages/sdk/src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:3001/v1";
+export const WALLET_SERVICE_API_URL = "http://localhost:3001/v1";
 
--- packages/sdk/src/nft/index.ts
+++ packages/sdk/src/nft/index.ts
@@ -1,45 +1,45 @@
 import { TransactionReceiptsBlockHash } from "alchemy-sdk";
 import axios from "axios";
 import { Wallet } from "ethers";
-import { API_URL } from "../constants";
+import { WALLET_SERVICE_API_URL } from "../constants";
 import { CreateERC1155CollectionRequest, CreateERC721CollectionRequest, MintERC1155Request, MintERC721Request, Nft, TransferRequest } from "../types";
 
   export async function getNfts(address: string): Promise<Nft[]> {
-    const result = await axios.get(`${API_URL}/nft/collectibles/${address}`);
+    const result = await axios.get(`${WALLET_SERVICE_API_URL}/nft/collectibles/${address}`);
     return result.data;
   }
 
   export async function transferNft(transferRequest: TransferRequest, pk: string): Promise<string> {
-    const unsignedTxResponse = await axios.post(`${API_URL}/nft/transfer/unsigned-transactions`, transferRequest);
+    const unsignedTxResponse = await axios.post(`${WALLET_SERVICE_API_URL}/nft/transfer/unsigned-transactions`, transferRequest);
     const signedTx =  new Wallet(pk).signTransaction(unsignedTxResponse.data);
-    const txHashResponse = await axios.post(`${API_URL}/transactions/on-chain-transactions`, signedTx);
+    const txHashResponse = await axios.post(`${WALLET_SERVICE_API_URL}/transactions/on-chain-transactions`, signedTx);
     return txHashResponse.data;
   }
 
   export async function createERC1155Collection(creationRequest: CreateERC1155CollectionRequest, pk: string): Promise<string> {
-    const unsignedTxResponse = await axios.post(`${API_URL}/nft/erc1155/deployment/unsigned-transactions`, creationRequest);
+    const unsignedTxResponse = await axios.post(`${WALLET_SERVICE_API_URL}/nft/erc1155/deployment/unsigned-transactions`, creationRequest);
     const signedTx =  new Wallet(pk).signTransaction(unsignedTxResponse.data);
-    const txHashResponse = await axios.post(`${API_URL}/transactions/on-chain-transactions`, signedTx);
+    const txHashResponse = await axios.post(`${WALLET_SERVICE_API_URL}/transactions/on-chain-transactions`, signedTx);
     return txHashResponse.data;
   }
 
   export async function createERC721Collection(creationRequest: CreateERC721CollectionRequest, pk: string): Promise<string> {
-    const unsignedTxResponse = await axios.post(`${API_URL}/nft/erc721/deployment/unsigned-transactions`, creationRequest);
+    const unsignedTxResponse = await axios.post(`${WALLET_SERVICE_API_URL}/nft/erc721/deployment/unsigned-transactions`, creationRequest);
     const signedTx =  new Wallet(pk).signTransaction(unsignedTxResponse.data);
-    const txHashResponse = await axios.post(`${API_URL}/transactions/on-chain-transactions`, signedTx);
+    const txHashResponse = await axios.post(`${WALLET_SERVICE_API_URL}/transactions/on-chain-transactions`, signedTx);
     return txHashResponse.data;
   }
 
   export async function mintERC1155Token(mintRequest: MintERC1155Request, pk: string): Promise<string> {
-    const unsignedTxResponse = await axios.post(`${API_URL}/nft/erc1155/mint/unsigned-transactions`, mintRequest);
+    const unsignedTxResponse = await axios.post(`${WALLET_SERVICE_API_URL}/nft/erc1155/mint/unsigned-transactions`, mintRequest);
     const signedTx =  new Wallet(pk).signTransaction(unsignedTxResponse.data);
-    const txHashResponse = await axios.post(`${API_URL}/transactions/on-chain-transactions`, signedTx);
+    const txHashResponse = await axios.post(`${WALLET_SERVICE_API_URL}/transactions/on-chain-transactions`, signedTx);
     return txHashResponse.data;
   }
 
   export async function mintERC721Token(mintRequest: MintERC721Request, pk: string): Promise<string> {
-    const unsignedTxResponse = await axios.post(`${API_URL}/nft/erc721/deployment/unsigned-transactions`, mintRequest);
+    const unsignedTxResponse = await axios.post(`${WALLET_SERVICE_API_URL}/nft/erc721/deployment/unsigned-transactions`, mintRequest);
     const signedTx =  new Wallet(pk).signTransaction(unsignedTxResponse.data);
-    const txHashResponse = await axios.post(`${API_URL}/transactions/on-chain-transactions`, signedTx);
+    const txHashResponse = await axios.post(`${WALLET_SERVICE_API_URL}/transactions/on-chain-transactions`, signedTx);
     return txHashResponse.data;
   }
