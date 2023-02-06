73b00f7bf06e56e141d46e93c5d7fd5601aa355c
Johanna Fransson <adrina0002@gmail.com>
2023-02-06 17:49:17 +0000

    add some styling to the modal

--- packages/ui/LoginComponent/LoginModal.tsx
+++ packages/ui/LoginComponent/LoginModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import "../custom.css";
 
 interface ModalProps {
   children: React.ReactNode;
@@ -10,22 +11,42 @@ interface ModalProps {
 
 export const LoginModal: React.FC<ModalProps> = (props) => {
   const { children, onAction, show } = props;
+
+  const metamaskLogin = async () => {
+    try {
+      const provider = "PROVIDER STRING HERE";
+      //TODO: install ethers in this package
+      const web3Provider = new ethers.providers.Web3Provider(provider!);
+      const signer = web3Provider.getSigner();
+      const gotAccount = await signer.getAddress();
+      const network = await web3Provider.getNetwork();
+      console.info(`EOA Address ${gotAccount}\nNetwork: ${network}`);
+      //this.provider = provider;
+      return provider;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
   return (
     <Modal show={show} onHide={() => onAction(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        Woohoo, you're reading this text in a modal! {children}
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => onAction(false)}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={() => onAction(false)}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+      <div className="modalContainer">
+        <p className="modalTitle">Sign-in or register</p>
+
+        <Button className="modalButtonSignIn">[Social Login] tKey </Button>
+        <br></br>
+        <br></br>
+
+        <Button className="modalButtonSignIn">[Social Login] tKey </Button>
+        <br></br>
+        <br></br>
+        <p className="modalOr">or</p>
+
+        <p className="modalConnectWallet">Connect a Wallet</p>
+
+        <p className="modalOr">Powered by [LOGO] Liquality</p>
+        <p className="modalTerms">Terms & Conditions</p>
+      </div>
     </Modal>
   );
 };
